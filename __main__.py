2794ff267ffd9c2581aa378b9c35b7dfcd7481df
PaulBayfield <137620008+PaulBayfield@users.noreply.github.com>
2024-11-26 23:26:40 +0100

    feat: Added main file

---
+++ __main__.py
@@ -0,0 +1,24 @@
+import asyncio
+
+from CROUStillantBot.bot import Bot
+from os import environ
+from dotenv import load_dotenv
+from aiohttp import ClientSession
+
+
+load_dotenv(dotenv_path=f".env")
+
+
+async def main():
+    """
+    Lance le bot
+    """
+    client = Bot()
+
+    async with ClientSession() as session:
+        async with client:
+            client.session = session
+            await client.start(environ["TOKEN"], reconnect=True)
+
+
+asyncio.run(main())
