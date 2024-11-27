3fd39fa0833ec8d1f08dce77a9cb5b1c9cad3cfb
PaulBayfield <137620008+PaulBayfield@users.noreply.github.com>
2024-11-27 22:20:35 +0100

    feat: Added private commands to manage the bog

--- CROUStillantBot/cogs/Development.py
+++ CROUStillantBot/cogs/Development.py
@@ -1,5 +1,4 @@
-import discord
-from discord import app_commands
+from ..utils.functions import getLogEmoji
 from discord.ext import commands
 
 
@@ -23,6 +22,52 @@ class Development(commands.Cog):
     async def logout(self, ctx: commands.Context):
         await ctx.send("Logging out...")
         await self.client.close()
+
+
+    @commands.command(help="reload", hidden=True)
+    @commands.is_owner()
+    async def reload(self, ctx: commands.Context, cog: str):
+        try:
+            self.client.reload_extension(f"cogs.{cog}")
+        except Exception as e:
+            await ctx.send(f"Impossible de recharger le module `{cog}` : `{e}`")
+            
+        await ctx.send(f"Module `{cog}` rechargé")
+
+
+    @commands.command(help="load", hidden=True)
+    @commands.is_owner()
+    async def load(self, ctx: commands.Context, cog: str):
+        try:
+            self.client.load_extension(f"cogs.{cog}")
+        except Exception as e:
+            await ctx.send(f"Impossible de charger le module `{cog}` : `{e}`")
+            
+        await ctx.send(f"Module `{cog}` chargé")
+
+
+    @commands.command(help="unload", hidden=True)
+    @commands.is_owner()
+    async def unload(self, ctx: commands.Context, cog: str):
+        try:
+            self.client.unload_extension(f"cogs.{cog}")
+        except Exception as e:
+            await ctx.send(f"Impossible de décharger le module `{cog}` : `{e}`")
+            
+        await ctx.send(f"Module `{cog}` déchargé")
+        
+
+    @commands.command(help="logs", hidden=True)
+    @commands.is_owner()
+    async def logs(self, ctx: commands.Context):
+        logs = await self.client.entities.logs.getLast(ctx.guild.id, 50)
+
+        text = ""
+        for log in logs:
+            text += f"• {getLogEmoji(log.get('idtpl'))} `{log.get('date').strftime('%d/%m/%Y %H:%M:%S')}` • {log.get('message')}\n"
+
+        await ctx.author.send(text)
+        await ctx.reply("50 derniers logs ont été envoyés en message privé.")
 
 
 async def setup(client: commands.Bot):
